9fd04000fac51efb41756edd7b50217cd86d664a
Mohammed Noori <kmanoorib@gmail.com>
2019-08-09 21:57:38 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,9 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    # https://lostisland.github.io/faraday/usage/
+    # Make a simple GET request by requiring the Faraday gem and using Faraday.get:
+    # response = Faraday.get 'http://sushi.com/nigiri/sake.json'
     resp = Faraday.get("https://api.github.com/search/repositories") do |req|
       # binding.pry
       # params = {"query"=>"tetris", "controller"=>"repositories", "action"=>"github_search"}
@@ -12,6 +15,9 @@ class RepositoriesController < ApplicationController
       req.params["client_id"] = '' # get it from github
       req.params["client_secret"] = ''
     end
-    binding.pry
+    # binding.pry
+    body = JSON.parse(resp.body)
+    @items = body["items"]
+    render :search
   end
 end
