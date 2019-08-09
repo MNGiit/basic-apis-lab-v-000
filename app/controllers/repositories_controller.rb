5fab344aa4f2eb378df101619602279a56caccf8
Mohammed Noori <kmanoorib@gmail.com>
2019-08-09 21:48:41 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,12 @@ class RepositoriesController < ApplicationController
 
   def github_search
     resp = Faraday.get("https://api.github.com/search/repositories") do |req|
-      binding.pry
+      # binding.pry
+      # params = {"query"=>"tetris", "controller"=>"repositories", "action"=>"github_search"}
+      req.params["q"] = params[:query]
+      req.params["client_id"] = '' # get it from github
+      req.params["client_secret"] = ''
     end
+    
   end
 end
